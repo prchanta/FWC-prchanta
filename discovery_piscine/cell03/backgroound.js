9eae68048bcb2a0452ca1bc64323d0b152e2dcf7
prchanta <68100401@kmitl.ac.th>
2026-09-18 14:58:35 +0700

    Add function to change background color randomly

---
+++ discovery_piscine/cell03/backgroound.js
@@ -0,0 +1,7 @@
+function changeBackground() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+}
