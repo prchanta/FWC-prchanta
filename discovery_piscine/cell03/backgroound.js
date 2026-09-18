fc62eae3bae887ea92bf2d8f8856824ab29a506e
prchanta <68100401@kmitl.ac.th>
2026-09-18 14:17:51 +0700

    Delete discovery_piscine/cell03/backgroound.js

--- discovery_piscine/cell03/backgroound.js
+++
@@ -1,7 +0,0 @@
-function changeBackground() {
-    const r = Math.floor(Math.random() * 256);
-    const g = Math.floor(Math.random() * 256);
-    const b = Math.floor(Math.random() * 256);
-
-    document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-}
